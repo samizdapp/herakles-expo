6b862ab9b5e586e43fa05d3de087ad98b03897ef
Ryan Bennett <nomad.ry@gmail.com>
2022-05-22 06:08:31 -0600

    set dev IP address

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
+import Constants from "expo-constants";
 
-export default function App() {
+export default function App({ devIP }) {
   return (
     <WebView
       style={styles.container}
-      source={{ uri: "http://setup.local" }}
+      source={{ uri: `http://${devIP}` }}
       onMessage={(e) => {}}
     />
   );
 }
+
+App.defaultProps = {
+  devIP: Constants.manifest.extra.devIP || "setup.local",
+};
 
 const styles = StyleSheet.create({
   container: {
---
+++ app.config.js
@@ -0,0 +1,9 @@
+import "dotenv/config";
+
+export default {
+  name: "Herakles",
+  version: "1.0.0",
+  extra: {
+    devIP: process.env.DEV_IP,
+  },
+};
